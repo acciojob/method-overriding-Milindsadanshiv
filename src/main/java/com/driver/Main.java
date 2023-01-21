ac3d4e5c496b1eb4bde7bfeb1f9813c24eb8c297
Milindsadanshiv <milindsadanshiv33@gmail.com>
2023-01-21 21:50:16 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,3 @@ public class Main {
        //System.out.println(obj.meth());
     }
 }
-
-class A{
-        public String meth() {
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-         public String meth(){
-         return "Method is overridden in Extendend class B";
-     }
-}
